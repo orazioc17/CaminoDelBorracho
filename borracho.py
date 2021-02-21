39ea30e3e263b5f8d93d968c9663c09612a5066c
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2021-02-21 10:24:56 -0400

    Se crearon las clases principales de la simulacion

---
+++ borracho.py
@@ -0,0 +1,25 @@
+import random
+
+
+class Borracho():
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+
+class BorrachoTradicional(Borracho):
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+    def camina():
+        return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+    
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
