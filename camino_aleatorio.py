cdf1cf7f2e457ec91ff3dc5cbe2d0b13c7b424a8
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2021-02-22 09:06:07 -0400

    Logre hacer una grafica del movimiento de un borracho

--- camino_aleatorio.py
+++ camino_aleatorio.py
@@ -4,6 +4,18 @@ from campo import Campo
 from coordenada import Coordenada
 from bokeh.plotting import figure, show
 
+# Manejo de la grafica con bokeh
+def graficar (x,y):
+    grafica = figure(title = 'Camino aleatorio', x_axis_label= 'pasos', y_axis_label = 'Distancia')
+    grafica.line(x, y, legend_label = 'Distancia media')
+    show(grafica)
+
+
+def graficar_2(x, y):
+    grafica = figure()
+    grafica.line(x,y)
+    show(grafica)
+
 
 def caminata(campo, borracho, pasos):
     inicio = campo.obtener_coordenada(borracho)
@@ -11,7 +23,22 @@ def caminata(campo, borracho, pasos):
     for _ in range(pasos):
         campo.mover_borracho(borracho)
     
+    
     return inicio.distancia(campo.obtener_coordenada(borracho))
+
+
+def caminata_grafica(campo, borracho, pasos):
+    inicio = campo.obtener_coordenada(borracho)
+    x_arreglo = []
+    y_arreglo = []
+    x_arreglo.append(0)
+    y_arreglo.append(0)
+    #Ciclo para mover al borracho por el plano cartesiano
+    for _ in range(pasos):
+        campo.mover_borracho(borracho)
+        x_arreglo.append(campo.obtener_coordenada(borracho).x)
+        y_arreglo.append(campo.obtener_coordenada(borracho).y)
+    graficar_2(x_arreglo, y_arreglo)
 
 
 def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
@@ -24,19 +51,18 @@ def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
         campo.anadir_borracho(borracho, origen)
         simulacion_caminata = caminata(campo, borracho, pasos)
         distancias.append(round(simulacion_caminata, 1))
-    
     return distancias
-        
-# Manejo de la grafica con bokeh
-def graficar (x,y):
-    grafica = figure(title = 'Camino aleatorio', x_axis_label= 'pasos', y_axis_label = 'Distancia')
-    grafica.line(x, y, legend_label = 'Distancia media')
-
-    show(grafica)
 
 
 def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+    #Hacer una grafica del movimiento de un solo borracho
     distancias_media_por_caminata = []
+    campo = Campo()
+    borracho = tipo_de_borracho(nombre = 'Orazio')
+    inicio = Coordenada(0,0)
+    campo.anadir_borracho(borracho, inicio)
+    distancia = 100000
+    caminata_grafica(campo, borracho,distancia)
     #Ciclo para cada cantidad de pasos
     for pasos in distancias_de_caminata:
         distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
