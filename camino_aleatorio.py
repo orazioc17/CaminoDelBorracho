df3273a47f1542bf857857f067627d48946b698c
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2021-02-22 10:20:22 -0400

    Agregada interactividad con el usuario

--- camino_aleatorio.py
+++ camino_aleatorio.py
@@ -22,17 +22,12 @@ def caminata(campo, borracho, pasos):
     #Ciclo para mover al borracho por el plano cartesiano
     for _ in range(pasos):
         campo.mover_borracho(borracho)
-    
-    
     return inicio.distancia(campo.obtener_coordenada(borracho))
 
 
 def caminata_grafica(campo, borracho, pasos):
-    inicio = campo.obtener_coordenada(borracho)
-    x_arreglo = []
-    y_arreglo = []
-    x_arreglo.append(0)
-    y_arreglo.append(0)
+    x_arreglo = [0]
+    y_arreglo = [0]
     #Ciclo para mover al borracho por el plano cartesiano
     for _ in range(pasos):
         campo.mover_borracho(borracho)
@@ -54,15 +49,18 @@ def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
     return distancias
 
 
-def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
-    #Hacer una grafica del movimiento de un solo borracho
-    distancias_media_por_caminata = []
+#Hacer una grafica del movimiento de un solo borracho
+def un_borracho(tipo_de_borracho):
     campo = Campo()
     borracho = tipo_de_borracho(nombre = 'Orazio')
     inicio = Coordenada(0,0)
     campo.anadir_borracho(borracho, inicio)
-    distancia = 100000
-    caminata_grafica(campo, borracho,distancia)
+    distancia = int(input('Ingresa cuantos pasos dara el borracho: '))
+    caminata_grafica(campo, borracho, distancia)
+
+
+def varias_repeticiones(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+    distancias_media_por_caminata = []
     #Ciclo para cada cantidad de pasos
     for pasos in distancias_de_caminata:
         distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
@@ -76,8 +74,23 @@ def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
         print(f'minima = {distancia_minima}')
     graficar(distancias_de_caminata, distancias_media_por_caminata)
 
+
+def main():
+    opcion = 0 
+    print('Bienvenido/a al camino de los borrachos')
+    print('Si ingresas 1, podras ver el camino de un borracho')
+    print('Si ingresas 2, podras comparar como se comporta un borracho con muchas simulaciones')
+    while(opcion <1 or opcion >2):
+        opcion = int(input('Ingresa la opcion correspondiente: '))
+    if opcion == 1:
+        un_borracho(BorrachoTradicional)
+    else:
+        print('Se haran 4 simulaciones con distancias de 10, 100, 1000, 10000 pasos')
+        numero_de_intentos = int(input('Ingrese el numero de intentos: '))
+        distancias_de_caminata = [10, 100, 1000, 10000]
+        varias_repeticiones(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
+
+
 # Entry point
 if __name__ == '__main__':
-    distancias_de_caminata = [10, 100, 1000, 10000]
-    numero_de_intentos = 100
-    main(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
+    main()
